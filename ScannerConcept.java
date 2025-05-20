1197aa9b0ed1428568d301a2d43dcb1aa996e9c6
RAJA <raja.mca@icloud.com>
2025-05-20 20:43:03 +0530

    scanner updated

---
+++ ScannerConcept.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ScannerConcept {
+    public static void main(String[] args) {
+        // System.out.println(Arrays.toString(args));
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Your Name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter Your Tamil Mars:");
+        int tamilMark = scanner.nextInt();
+
+        System.out.println("Enter Your English Mars:");
+        int englishMark = scanner.nextInt();
+
+        System.out.println("Enter Your Maths Mars:");
+        int mathsMark = scanner.nextInt();
+
+        System.out.println("Enter Your Science Mars:");
+        int scienceMark = scanner.nextInt();
+
+        System.out.println("Enter Your Social Mars:");
+        int socialMark = scanner.nextInt();
+        
+        System.out.println("==============================================");
+        System.out.println("Hello " + name + ", Your Details are below");
+        System.out.println("Total Marks: " + (tamilMark + englishMark + mathsMark + scienceMark + socialMark));
+        System.out.println("Average Marks: " + ((tamilMark + englishMark + mathsMark + scienceMark + socialMark) / 5));
+        System.out.println("Percentage: " + ((tamilMark + englishMark + mathsMark + scienceMark + socialMark) / 5) * 100 / 100);
+
+    }
+}
